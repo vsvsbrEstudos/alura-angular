6764498d4442277b9a334f1fc9eb1a21645fd49f
Vagner Santos <eu@vagnersantos.com>
2016-09-04 14:33:27 -0300

    Diretivas buscando dados - títulos.

--- public/js/directives/minhas-diretivas.js
+++ public/js/directives/minhas-diretivas.js
@@ -1,4 +1,4 @@
-angular.module('minhasDiretivas', [])
+angular.module('minhasDiretivas', ['meusServicos'])
 .directive('meuPainel', function(){
   var ddo = {};
 
@@ -53,4 +53,20 @@ angular.module('minhasDiretivas', [])
   };
 
   return ddo;
-});
+})
+.directive('meusTitulos', function() {
+  var ddo = {};
+
+  ddo.restrict = 'E';
+  ddo.template = '<ul><li ng-repeat="titulo in titulos">{{titulo}}</li></ul>';
+
+  ddo.controller = function($scope, recursoFoto) {
+    recursoFoto.query(function(fotos){
+      $scope.titulos = fotos.map(function (foto) {
+        return foto.titulo;
+      });
+    });
+  };
+
+  return ddo;
+});;
